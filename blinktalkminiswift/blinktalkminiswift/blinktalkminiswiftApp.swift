e0e567963a52b69ec3def304b112fc97f6b5f8fc
Shruti-Nagawekar <snagawek@ttu.edu>
2025-11-16 19:49:19 -0600

    Basics swift setup

---
+++ blinktalkminiswift/blinktalkminiswift/blinktalkminiswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  blinktalkminiswiftApp.swift
+//  blinktalkminiswift
+//
+//  Created by Shruti on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct blinktalkminiswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
